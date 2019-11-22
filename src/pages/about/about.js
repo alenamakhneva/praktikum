a785418903ca48daed821e6e05b70c5c91e692ee
Alena Makhneva <alenamakhneva@yandex.com>
2019-11-22 09:23:43 +0300

    adaptive pages

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,12 +1,16 @@
 import './about.css';
-import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
+import Glide, {
+  Controls,
+  Breakpoints
+} from '@glidejs/glide/dist/glide.modular.esm'
 
 new Glide('.glide', {
-    type: 'carousel',
-    startAt: 0,
-    perView: 3,
-    peek: 20,
-    gap: 16,
-  }).mount({ Controls, Breakpoints }) 
-
-
+  type: 'carousel',
+  startAt: 0,
+  perView: 3,
+  peek: 88,
+  gap: 16,
+}).mount({
+  Controls,
+  Breakpoints
+})
