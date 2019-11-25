79f1625c492bf630a85ae8cbcf599dc585beaa69
Alena Makhneva <alenamakhneva@yandex.com>
2019-11-25 16:55:38 +0300

    level-1 review-3

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 
 
@@ -29,17 +28,16 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: [
-                    isDev ? 'style-loader' :  
-                    {
+                    isDev ? 'style-loader' : {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                             publicPath: '../'
+                            publicPath: '../'
                         }
                     },
                     'css-loader',
                     'postcss-loader'
                 ]
-            
+
             },
             {
                 test: /\.(png|jpg|gif|ico|svg)$/,
@@ -63,7 +61,7 @@ module.exports = {
             assetNameRegExp: /\.css$/g,
             cssProcessor: require('cssnano'),
             cssProcessorPluginOptions: {
-                    preset: ['default'],
+                preset: ['default'],
             },
             canPrint: true
         }),
@@ -72,33 +70,23 @@ module.exports = {
             hash: true,
             template: './src/pages/index/index.html',
             filename: 'index.html'
-          }),
+        }),
         new HtmlWebpackPlugin({
             inject: false,
             hash: true,
             template: './src/pages/about/about.html',
             filename: 'about.html'
-          }),
+        }),
         new HtmlWebpackPlugin({
             inject: false,
             hash: true,
             template: './src/pages/analytics/analytics.html',
             filename: 'analytics.html'
-          }),
+        }),
         new WebpackMd5Hash(),
         new webpack.DefinePlugin({
             'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         }),
 
-        new UglifyJsPlugin({
-            uglifyOptions: {
-                compress: {
-                    global_defs: {
-                        "@alert": "console.log",
-                    },
-                    drop_console: true
-                }
-            }
-        })
     ]
 };
